dd772f345dcb36613f7e6c64540c6dd12f1b6f1e
Daniel Ho <danielho54@gmail.com>
2017-03-25 14:16:14 -0700

    say function

--- say/say.py
+++ say/say.py
@@ -1,9 +1,9 @@
 from gtts import gTTS
 import os
 say = ''
-while(say != 'Quit'):
+while(say != 'quit'):
 	say = input('What to Say: ')
-	tts = gTTS(say, 'es')
-	tts.save('test.mp3')
-	os.system('start "" test.mp3')
+	tts = gTTS(say, 'en')
+	tts.save('say.mp3')
+	os.system('say.mp3')
 	os.system('')
