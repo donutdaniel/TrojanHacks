ebb8986068377ce114bd480233549d66de9289fa
Daniel Ho <danielho54@gmail.com>
2017-03-25 20:29:47 -0700

    more server stuff

--- mySite/hackathons/views.py
+++ mySite/hackathons/views.py
@@ -2,6 +2,28 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from .models import Review
+import os
+import re
+
+#need to parse out
+
+hackathonsList = {}
+
+class Hackathon:
+	def __init__(self, url, name, date, location):
+		self.url = url
+		self.name = name
+		self.date = date
+		self.location = location
+
+def parse(data):
+	count = 0
+	for line in data:
+		temp = re.search(' ', line)
+		hackathonsList.append(Hackathon(temp.group(0), temp.group(1), temp.group(2), temp.group(3)))
+
+#file = open('data.txt', 'r')
+#parse(file)
 
 def index(request):
     latest_review_list = Review.objects.order_by('-pub_date')[:5]
@@ -14,4 +36,3 @@ def detail(request, review_id):
 
 def parse(str):
 	return ''
-
