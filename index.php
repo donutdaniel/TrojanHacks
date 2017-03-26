645a214e2544441c96d4303c7feaf5c6e131cf39
adibellathur <bellathu@usc.edu>
2017-03-25 21:25:05 -0700

    trying this

--- index.php
+++ index.php
@@ -3,11 +3,17 @@ include_once('simple_html_dom.php');
 
 $html = file_get_html('http://mlh.io/seasons/na-2017/events');
 
-// Find all images 
-foreach($html->find('img') as $element)
- 		echo $element . '<br>';
+// $classname="blockProduct";
+// $finder = new DomXPath($doc);
+// $spaner = $finder->query("//*[contains(@class, '$classname')]");
 
-foreach($html->find('') as $name)
+// Find all images 
+// foreach($html->find('img') as $element)
+//  		echo $element . '<br>';
+
+$counter = array(10);
+foreach($html->find('h3') as $element)
+			echo $element;
 
 ?>
 
@@ -17,6 +23,9 @@ foreach($html->find('') as $name)
 	<title>Parser Example</title>
 </head>
 <body>
-
+		Parser
 </body>
 </html>
+
+
+
